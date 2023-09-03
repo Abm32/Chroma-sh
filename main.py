b56dc5ba427a48a5924c40a6a38c1a9d16637bf3
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-09-03 16:25:38 +0530

    simple diary entry app using tkinter

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
-import chromadb
+import tkinter as tk
+from tkinter import messagebox
 
-client = chromadb.Client()
-collection = client.create_collection("yt_demo")
+# Function to save the diary entry to a file
+def save_entry():
+    entry_text = entry.get("1.0", "end-1c")
+    if entry_text.strip():
+        with open("diary.txt", "a") as diary_file:
+            diary_file.write(entry_text + "\n")
+        messagebox.showinfo("Success", "Diary entry saved successfully!")
+        entry.delete("1.0", "end")
 
-collection.add(
-    documents=["This is a document about cat", "This is a document about car"],
-    metadatas=[{"category": "animal"}, {"category": "vehicle"}],
-    ids=["id1", "id2"]
-)
+# Function to view previous diary entries
+def view_entries():
+    try:
+        with open("diary.txt", "r") as diary_file:
+            entries = diary_file.readlines()
+            if entries:
+                view_window = tk.Toplevel(root)
+                view_window.title("Diary Entries")
 
-results = collection.query(
-    query_texts=["vehicle"],
-    n_results=1
-)
-print(results)
+                scrollbar = tk.Scrollbar(view_window)
+                scrollbar.pack(side="right", fill="y")
 
-import os
+                text_box = tk.Text(view_window, wrap="word", yscrollcommand=scrollbar.set)
+                text_box.pack()
 
-def read_files_from_folder(folder_path):
-    file_data = []
+                scrollbar.config(command=text_box.yview)
 
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".txt"):
-            with open(os.path.join(folder_path, file_name), 'r') as file:
-                content = file.read()
-                file_data.append({"file_name": file_name, "content": content})
+                for entry in entries:
+                    text_box.insert("end", entry)
 
-    return file_data
+                text_box.config(state="disabled")
+            else:
+                messagebox.showinfo("No Entries", "No diary entries found.")
+    except FileNotFoundError:
+        messagebox.showinfo("No Entries", "No diary entries found.")
 
-folder_path = "pets"
-file_data = read_files_from_folder(folder_path)
+# Create the main application window
+root = tk.Tk()
+root.title("Personal Diary App")
 
-documents = []
-metadatas = []
-ids = []
+# Create a text box for entering diary entries
+entry = tk.Text(root, wrap="word", width=40, height=10)
+entry.pack(pady=10)
 
-for index, data in enumerate(file_data):
-    documents.append(data['content'])
-    metadatas.append({'source': data['file_name']})
-    ids.append(str(index + 1))
+# Create buttons to save and view diary entries
+save_button = tk.Button(root, text="Save Entry", command=save_entry)
+view_button = tk.Button(root, text="View Entries", command=view_entries)
+save_button.pack()
+view_button.pack()
 
-pet_collection = client.create_collection("pet_collection")
-
-pet_collection.add(
-    documents=documents,
-    metadatas=metadatas,
-    ids=ids
-)
+# Run the Tkinter main loop
+root.mainloop()
